ecff98e705f3ab4802ae212cbe34b392f6cc2cac
Norbert Olesiak-Zbieg <nolesiak@gabos.pl>
2021-05-20 14:03:19 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POK_ZAD2
+{
+    public class Circle
+    {
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        private double circuit;
+        private double field;
+        public double Circuit
+        {
+            get
+            {
+                circuit = CountCircuit(Radius);
+                return circuit;
+            }
+        }
+        public double Field
+        {
+            get
+            {
+                field = CountField(Radius);
+                return field;
+            }
+        }
+
+        public double Radius { get; set; }
+
+        double CountCircuit(double radius)
+        {
+            var circuit = 2 * Math.PI * radius;
+            return circuit;
+        }
+        double CountField(double radius)
+        {
+            var field = Math.PI * Math.Pow(Radius, 2);
+            return field;
+        }
+        public override string ToString()
+        {
+            return "Circle Radius is: " + Radius + ", Circle Field is: " + Field + ", Circle Circuit is: " + Circuit;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace POK_ZAD2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var circle1 = new Circle(2);
+            Console.WriteLine("Circle 1");
+            Console.WriteLine(circle1.ToString());
+        }
+    }
+}
